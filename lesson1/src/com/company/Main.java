40742113566742e24795fd7a6c542b7740146f64
natalia zhydejkina <Karina.Varlena@instantconnectnow.com>
2020-09-12 22:35:36 +0300

    lesson1

---
+++ lesson1/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	float f=3.14f;
+	int a=568654;
+	int a2;
+	a2=3;
+	char c ='c';
+	long v = 4987654567890l;
+	byte h=126;
+	boolean m= true;
+	short s= 7809;
+	String cc="hgdhuiu";
+
+	int sum= a+a2;
+    int diff=a-a2;
+    double div=a/a2;
+    int mult=a*a2;
+    int hb=2004+8+11;
+        System.out.println(mult);
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(div);
+        System.out.println(hb);
+    }
+}
